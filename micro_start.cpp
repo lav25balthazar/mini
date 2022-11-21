06ff88057fcc2bce99d32aec702a91d5b6545761
lav25balthazar <118749391+lav25balthazar@users.noreply.github.com>
2022-11-21 20:28:28 -0300

    Create micro_start.cpp

---
+++ micro_start.cpp
@@ -0,0 +1,18 @@
+#include "micro_start.h"
+#include <iostream>
+
+using namespace std;
+//start 5v quando lê algo; stop 0 
+uStart::uStart(int signal_pin){
+  this->signal_pin = signal_pin;
+  this->state = uStartState::STOP;
+}
+void uStart::update(){
+  bool leDigitalPin = digitalRead(this->signal_pin);
+  if (leDigitalPin == 0)
+    this->state = uStartState::STOP;
+  else
+    this->state = uStartState::START;
+  
+  
+}
