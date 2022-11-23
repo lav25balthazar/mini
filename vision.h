22bca6eee5b1c25d158b0c7c93acd43b2662816b
Lavínia Balthazar <118749391+lav25balthazar@users.noreply.github.com>
2022-11-23 10:01:27 -0300

    Create vision.h

---
+++ vision.h
@@ -0,0 +1,33 @@
+#ifndef VISION_H
+#define VISION_H
+
+#include "analog_sensor.h"
+#include "digital_sensor.h"
+
+namespace EnemyPosition
+{
+    enum Pos
+    {
+        LEFT,
+        RIGHT,
+        FRONT,
+        FULL_LEFT,
+        FULL_RIGHT,
+        SEARCH_LEFT,
+        SEARCH_RIGHT
+    };
+}
+
+class Vision
+{
+public:
+    EnemyPosition::Pos enemy_position;
+    EnemyPosition::Pos latest_enemy_position;
+    void updateEnemyPosition(DigitalSensor &front_sensor,
+    DigitalSensor &full_left_sensor,
+    DigitalSensor &full_right_sensor, 
+    AnalogSensor &left_sensor, 
+    AnalogSensor &right_sensor);
+};
+
+#endif
