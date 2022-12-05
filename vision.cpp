a16849930961f3721a8ce9a7ed43e8ea05bd3f0a
Lavínia Balthazar <118749391+lav25balthazar@users.noreply.github.com>
2022-12-05 10:07:16 -0300

    Update vision.cpp
    corrigi erros de compilação

--- vision.cpp
+++ vision.cpp
@@ -11,37 +11,37 @@ void updateEnemyPosition(DigitalSensor &front_sensor,
 
   if (front_sensor.enemy_close != 0) {
     EnemyPosition::FRONT;
-//  Serial.println("ah, ta ali na frente");  /conectar arduino
+    Serial.println("ah, ta ali na frente");  
     return;
     }
 //  ve na frente
   if (right_sensor.distance_cm<20 && left_sensor.distance_cm > 20) {
       EnemyPosition:: RIGHT;
-//    Serial.println("ah, ta ali quase na direita");   /conectar arduino
+      Serial.println("ah, ta ali quase na direita");   
       return;  
       }
 //  ve na direita meio diagonal
   if (full_right_sensor.enemy_close != 0) {
        EnemyPosition::FULL_RIGHT;
-//     Serial.println("ah, ta ali na direita"); /conectar arduino
+       Serial.println("ah, ta ali na direita"); 
        return;
     }
 //  ve totalmente na direita
   if (full_left_sensor.enemy_close != 0) {
        EnemyPosition::FULL_LEFT;
-//     Serial.println("ah, ta ali na esquerda");   / conectar arduino
+       Serial.println("ah, ta ali na esquerda");  
        return;
    }
 // ve na esquerda
   if (left_sensor.distance_cm<20 && right_sensor.distance_cm>20) {
         EnemyPosition::LEFT;
-//      Serial.println("ah, ta ali quase na esquerda");  / conectar arduino
+        Serial.println("ah, ta ali quase na esquerda"); 
         return;
     }
   //ve quase na esquerda
   if (left_sensor.distance_cm<20 && right_sensor.distance_cm<20){
         EnemyPosition:: FRONT;
-//      Serial.println("muito perto, vendo com 2 sensores");   /conectar arduino
+        Serial.println("muito perto, vendo com 2 sensores");   
         return;
     }
   //oponente está muito perto, por isso os 2 sensores estão lendo e provavelmente o digital front também
