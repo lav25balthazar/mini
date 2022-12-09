268e85268abe4783ccfd4fed26479a4e42fb34a7
lavinia.balthazar@ice.ufjf.br <lavinia.balthazar@ice.ufjf.br>
2022-12-09 02:06:46 -0300

    agr vai dms

---
+++ micro_start.cpp
@@ -0,0 +1,19 @@
+#include "micro_start.h"
+#include <iostream>
+
+using namespace std;
+//start 5v quando lê algo; stop 0 
+uStart::uStart(int signal_pin){
+  this->signal_pin = signal_pin;
+  this->state = uStartState::STOP;
+}
+void uStart::update(){
+  Serial.println("estou na funcao do micro_strat uStart");
+  bool leDigitalPin = digitalRead(this->signal_pin);
+  if (leDigitalPin == 0)
+    this->state = uStartState::STOP;
+  else
+    this->state = uStartState::START;
+  
+  
+}
---
+++ vision.cpp
@@ -0,0 +1,55 @@
+#include "vision.h"
+#include <iostream>
+
+using namespace std;
+
+void updateEnemyPosition(DigitalSensor &front_sensor,
+    DigitalSensor &full_left_sensor,
+    DigitalSensor &full_right_sensor, 
+    AnalogSensor &left_sensor, 
+    AnalogSensor &right_sensor){
+
+  left_sensor.readSensor(); // lê sensores analogicos chamando 
+  right_sensor.readSensor() ; // a funcao readSensor do analog_sensor
+  front_sensor.readSensor(); // le sensores digitais chamando a 
+  full_left_sensor.readSensor(); // funcao readSensor
+  full_right_sensor.readSensor(); // do digital_sensor
+
+  Serial.println("estou em vision updateEnemyPosition");
+  if (front_sensor.enemy_close != 0) {
+    EnemyPosition::FRONT;
+    Serial.println("ah, ta ali na frente"); 
+    return;
+    }
+//  ve na frente
+  if (right_sensor.distance_cm<20 && left_sensor.distance_cm > 20) {
+      EnemyPosition::RIGHT;
+      Serial.println("ah, ta ali quase na direita");   
+      return;  
+      }
+//  ve na direita meio diagonal
+  if (full_right_sensor.enemy_close != 0) {
+       EnemyPosition::FULL_RIGHT;
+       Serial.println("ah, ta ali na direita"); 
+       return;
+    }
+//  ve totalmente na direita
+  if (full_left_sensor.enemy_close != 0) {
+       EnemyPosition::FULL_LEFT;
+       Serial.println("ah, ta ali na esquerda");   
+       return;
+   }
+// ve na esquerda
+  if (left_sensor.distance_cm<20 && right_sensor.distance_cm>20) {
+        EnemyPosition::LEFT;
+        Serial.println("ah, ta ali quase na esquerda");  
+        return;
+    }
+  //ve quase na esquerda
+  if (left_sensor.distance_cm<20 && right_sensor.distance_cm<20){
+        EnemyPosition:: FRONT;
+        Serial.println("muito perto, vendo com 2 sensores");   
+        return;
+    }
+  //oponente está muito perto, por isso os 2 sensores estão lendo e provavelmente o digital front também
+}
