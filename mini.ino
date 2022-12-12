9bb2a7a1b2bd68eec692d57ea9d263ec484ec296
lavinia.balthazar@ice.ufjf.br <lavinia.balthazar@ice.ufjf.br>
2022-12-12 18:17:27 -0300

    corrigindo erros de codigo da vision

--- mini.ino
+++ mini.ino
@@ -33,23 +33,25 @@ void setup()
 
 void loop() 
 {
- /*
+
   delay(1000);
-  Serial.println("RUN");
+  //Serial.println("RUN");
   digitalWrite(17, HIGH);
   digitalWrite(16, LOW);
-  ledcWrite(LEFT_MOTOR_CHANNEL, 255);
+//  ledcWrite(LEFT_MOTOR_CHANNEL, 255);
   //dacWrite(LEFT_MOTOR_PWM_PIN, 255);
-  delay(1000);
-  ledcWrite(LEFT_MOTOR_CHANNEL, 0);
+//  delay(1000);
+ // ledcWrite(LEFT_MOTOR_CHANNEL, 0);
   //dacWrite(LEFT_MOTOR_PWM_PIN, 0);1
-  delay(1000);
-  ledcWrite(LEFT_MOTOR_CHANNEL, 125);
+//  delay(1000);
+//  ledcWrite(LEFT_MOTOR_CHANNEL, 125);
   //dacWrite(LEFT_MOTOR_PWM_PIN, 0);
  */
 
   
-  static Robot robot;
-  robot.update();
+  static Robot aurora;
+  aurora.update();
+  aurora.readSensors();
+  
   // robot.updateVaiTomarNoCuIron();
 }
--- vision.cpp
+++ vision.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-void updateEnemyPosition(DigitalSensor &front_sensor,
+void Vision::updateEnemyPosition(DigitalSensor &front_sensor,
     DigitalSensor &full_left_sensor,
     DigitalSensor &full_right_sensor, 
     AnalogSensor &left_sensor, 
@@ -22,34 +22,36 @@ void updateEnemyPosition(DigitalSensor &front_sensor,
     return;
     }
 //  ve na frente
-  if (right_sensor.distance_cm<20 && left_sensor.distance_cm > 20) {
+  else if (right_sensor.distance_cm<20 && left_sensor.distance_cm > 20) {
       EnemyPosition::RIGHT;
       Serial.println("ah, ta ali quase na direita");   
       return;  
       }
 //  ve na direita meio diagonal
-  if (full_right_sensor.enemy_close != 0) {
+  else if (full_right_sensor.enemy_close != 0) {
        EnemyPosition::FULL_RIGHT;
        Serial.println("ah, ta ali na direita"); 
        return;
     }
 //  ve totalmente na direita
-  if (full_left_sensor.enemy_close != 0) {
+  else if (full_left_sensor.enemy_close != 0) {
        EnemyPosition::FULL_LEFT;
        Serial.println("ah, ta ali na esquerda");   
        return;
    }
 // ve na esquerda
-  if (left_sensor.distance_cm<20 && right_sensor.distance_cm>20) {
+  else if (left_sensor.distance_cm<20 && right_sensor.distance_cm>20) {
         EnemyPosition::LEFT;
         Serial.println("ah, ta ali quase na esquerda");  
         return;
     }
   //ve quase na esquerda
-  if (left_sensor.distance_cm<20 && right_sensor.distance_cm<20){
+  else if (left_sensor.distance_cm<20 && right_sensor.distance_cm<20){
         EnemyPosition:: FRONT;
         Serial.println("muito perto, vendo com 2 sensores");   
         return;
     }
   //oponente está muito perto, por isso os 2 sensores estão lendo e provavelmente o digital front também
+  else
+    Serial.println("nao está lendo nada");
 }
