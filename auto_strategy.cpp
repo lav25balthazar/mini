90f8fa05f39e024ce37f9a3b906fb7a9658f8b0a
lavinia.balthazar@ice.ufjf.br <lavinia.balthazar@ice.ufjf.br>
2023-01-22 21:12:02 -0300

    mudando potencia do front

--- auto_strategy.cpp
+++ auto_strategy.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-
+AutoStrategy autonoma;
 void AutoStrategy::updateMotors(Vision &vision, MotorControl &left_motor, MotorControl &right_motor){
     if(vision.enemy_position==EnemyPosition::RIGHT){
         left_motor.setPower(90);
@@ -12,16 +12,13 @@ void AutoStrategy::updateMotors(Vision &vision, MotorControl &left_motor, MotorC
         left_motor.setPower(20);
         right_motor.setPower(90);}
     else if(vision.enemy_position ==EnemyPosition::FRONT){
-        left_motor.setPower(70);
-        right_motor.setPower(70);}
+        left_motor.setPower(100);
+        right_motor.setPower(100);}
     else if(vision.enemy_position ==EnemyPosition::FULL_LEFT){
         left_motor.setPower(10);
         right_motor.setPower(95);}
     else if(vision.enemy_position ==EnemyPosition::FULL_RIGHT){
         left_motor.setPower(95);
         right_motor.setPower(10);}
-        
-      
-    
 };
     
