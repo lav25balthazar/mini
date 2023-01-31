f85d40f39b2b1704c96276c7c46eb59653a87e70
Lavínia Balthazar <118749391+lav25balthazar@users.noreply.github.com>
2023-01-31 16:14:11 +0000

    conversao  analog pra cm

--- analog_sensor.cpp
+++ analog_sensor.cpp
@@ -11,7 +11,7 @@ int AnalogSensor::readSensor() { //lê sensor analógico, dá uma leitura entre 
   //Serial.println("estou em analog_sensor, funcao readSensor");
   //Serial.println(raw_reading);
   if (raw_reading >= 0 && raw_reading <= 4095)
-    return (this->raw_reading*20.0)/2000.0;
+    return (int)((4095.0- (float)this->raw_reading)*(float)ANALOG_TO_CM);
   else
     return -1; // sensor com problema 
 
