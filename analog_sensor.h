ef3744afa6a5ca74c8c9af8f43839ae05a460988
Lavínia Balthazar <118749391+lav25balthazar@users.noreply.github.com>
2023-02-02 18:53:29 +0000

    tentando colocar o filtro

--- analog_sensor.h
+++ analog_sensor.h
@@ -14,5 +14,12 @@ public:
     int filterSensor();
     AnalogSensor(int pin);
 };
+private: // variaveis do filto
+  float _err_measure;
+  float _err_estimate;
+  float _q;
+  float _current_estimate = 0;
+  float _last_estimate = 0;
+  float _kalman_gain = 0;
 
 #endif
