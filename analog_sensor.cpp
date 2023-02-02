5255fd0dde462d171c1447c6aba5d08490af927e
Lavínia Balthazar <118749391+lav25balthazar@users.noreply.github.com>
2023-02-02 19:02:23 +0000

    continuando filtro

--- analog_sensor.cpp
+++ analog_sensor.cpp
@@ -18,8 +18,9 @@ int AnalogSensor::readSensor() { //lê sensor analógico, dá uma leitura entre 
     return -1; // sensor com problema 
 }
 int AnalogSensor::filterSensor(){ // tentando colocar o filtro do codigo antigo, tentando e falhando
+  // mea = this->raw_reading; ?
    _kalman_gain = _err_estimate/(_err_estimate + _err_measure);
-  _current_estimate = _last_estimate + _kalman_gain * (mea - _last_estimate);
+  _current_estimate = _last_estimate + _kalman_gain * (mea - _last_estimate); //mea: quanto que o sensor pode variar (ver isso)
   _err_estimate =  (1.0 - _kalman_gain)*_err_estimate + fabs(_last_estimate-_current_estimate)*_q;
   _last_estimate=_current_estimate;
 
