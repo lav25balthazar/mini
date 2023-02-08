8ef5c9d2f1d8b4f0a882c425cb82e35bde8d7ce1
Lavínia Balthazar <118749391+lav25balthazar@users.noreply.github.com>
2023-02-08 16:44:37 +0000

    corrigindo erros de lógica

--- robot.cpp
+++ robot.cpp
@@ -31,11 +31,11 @@ void Robot::readSensors() {
 void Robot::update() {
   uStart(MICRO_START_SIGNAL_PIN) = ustart;
   //Serial.println("estou na robot em funcao update");
-  if (uStartState::START) // gostaria de criar uma condição em que, no momento que o micro start manda sinal para começar, no micro_start.cpp, start é diferente de 0 p iniciar estrategia. 
-    this->robot_state = RobotState::INITIAL_STRATEGY;
-    while(uStartState::START){
-       aurora.readSensors();
-       this->robot_state = RobotState::AUTO_STRATEGY;}
+  if (ustart.state == uStartState::START){ // gostaria de criar uma condição em que, no momento que o micro start manda sinal para começar, no micro_start.cpp, start é diferente de 0 p iniciar estrategia. 
+    this->robot_state = RobotState::INITIAL_STRATEGY; // colocar estrategia inicial (que no caso não fiz ainda)
+    aurora.readSensors();
+    this->auto_strategy = autonoma.updateMotors(this->vision, this->left_motor, this->right_motor);
+    this->robot_state = RobotState::AUTO_STRATEGY;}
   else
     this->robot_state = RobotState::STOPPED;
 }
