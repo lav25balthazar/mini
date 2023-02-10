3036e5e5c373ed76ed89684bbafdaf20ec5b14ed
lavinia.balthazar@ice.ufjf.br <lavinia.balthazar@ice.ufjf.br>
2023-02-10 18:39:08 +0000

    corrigindo erros de compilação

--- vision.cpp
+++ vision.cpp
@@ -31,34 +31,35 @@ void Vision::updateEnemyPosition(DigitalSensor& front_sensor,
   //  ve na frente
   else if (right_sensor.distance_cm < 20 && left_sensor.distance_cm > 20) {
     this-> enemy_position =EnemyPosition::RIGHT;
-    //Serial.println("ah, ta ali quase na direita");
+    Serial.println("RIGHT");
     return;
   }
   //  ve na direita meio diagonal
   else if (full_right_sensor.enemy_close != 0) {
     this-> enemy_position =EnemyPosition::FULL_RIGHT;
-    //Serial.println("ah, ta ali na direita");
+    Serial.println("FULL_RIGHT");
     return;
   }
   //  ve totalmente na direita
   else if (full_left_sensor.enemy_close != 0) {
     this-> enemy_position =EnemyPosition::FULL_LEFT;
-    //Serial.println("ah, ta ali na esquerda");
+    Serial.println("FULL_LEFT");
     return;
   }
   // ve na esquerda
   else if (left_sensor.distance_cm < 20 && right_sensor.distance_cm>20) {
     this-> enemy_position =EnemyPosition::LEFT;
-    //Serial.println("ah, ta ali quase na esquerda");
+    Serial.println("LEFT");
     return;
   }
   //ve quase na esquerda
   else if (left_sensor.distance_cm < 20 && right_sensor.distance_cm < 20) {
     this-> enemy_position =EnemyPosition::FRONT;
-    //Serial.println("muito perto, vendo com 2 sensores");
+    Serial.println("FRONT");
     return;
   }
   //oponente está muito perto, por isso os 2 sensores estão lendo e provavelmente o digital front também
-  else
-    //Serial.println("nao está lendo nada");
+  else{
+    Serial.println("nao está lendo nada");
+  }
 }
