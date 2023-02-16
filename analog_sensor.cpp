b2326319fc3a165a1f5eac88729121692923f7e3
Lavínia Balthazar <118749391+lav25balthazar@users.noreply.github.com>
2023-02-16 17:47:09 +0000

    corrigindo variável

--- analog_sensor.cpp
+++ analog_sensor.cpp
@@ -10,9 +10,9 @@ AnalogSensor::AnalogSensor(int pin): filter(this->filter._err_measure, this->fil
 }
 int AnalogSensor::readSensor() { //lê sensor analógico, dá uma leitura entre 0 e 255/4095 se lê algo
   this->raw_reading = analogRead(this->pin);
-  this->raw_reading = this->filter.updateEstimate(this->raw_reading);
-  if (raw_reading >= 0 && raw_reading <= 4095)
-    return (int)((4095.0- (float)this->raw_reading)*(float)ANALOG_TO_CM); //conta do billy para converter (perguntar)
+  this->filtered_reading = this->filter.updateEstimate(this->raw_reading);
+  if (this->filtered_reading >= 0 && this->filtered_reading <= 4095)
+    return (int)((4095.0- (float)this->filtered_reading)*(float)ANALOG_TO_CM); //conta do billy para converter (perguntar)
   else
     return -1; // sensor com problema 
 }
